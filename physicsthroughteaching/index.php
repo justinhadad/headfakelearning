a7b63bb77e738be30e9b7ce820152d39be797ace
justinhadad <42585732+justinhadad@users.noreply.github.com>
2020-10-20 19:26:38 -0400

    Add files via upload

---
+++ physicsthroughteaching/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+		<META HTTP-EQUIV='CACHE-CONTROL' CONTENT='NO-CACHE'>
+		<meta name='rating' content='general'>
+		<link rel='stylesheet' href='/menu.css' type='text/css' media='screen' />
+		<link href='/rss/index.php' rel='alternate' type='application/rss+xml' title='News' />
+		<title>Physics Through Games</title>
+<style>
+
+* {box-sizing: border-box}
+body {
+  font-family: "Open Sans", sans-serif;
+  text-justify: inter-word;
+  margin-top: 50px;
+  margin-bottom: 100px;
+  margin-right: 100px;
+  margin-left: 100px;
+}
+
+button {
+  
+  font-family: "Open Sans", sans-serif;
+  font-size: 20px;
+  color: black; /* White text */
+  padding: 12px 24px; /* Some padding */
+  margin-top: 18px;
+  margin-left: 100px;
+  cursor: pointer; /* Pointer/hand icon */
+  float: left; /* Float the buttons side by side */
+  padding: 0;
+  border: none;
+  background: none;
+  text-align:justify;
+  padding: 1.2em 1em 0;
+  margin:1em;
+}
+
+button:after {
+  content:'';
+  display:inline-block;
+  width:99.5%;/* generates an extra transparent line */
+}
+
+/* Add a background color on hover */
+button:hover {
+  color: blue;
+}
+</style>
+
+<style type="text/css">
+      div#linkbar-upper a
+      {
+        color: #ffffff;
+      }
+    </style>
+    <style type="text/css">
+      span.theme-compatibility
+      {
+        display:    block;
+        font-style: italic;
+      }
+
+      ul#splash-theme-list
+      {
+        list-style-type: square;
+        margin-left:     0ex;
+        padding-left:    4ex;
+      }
+
+      ul#splash-theme-list li
+      {
+        clear:         both;
+        margin-bottom: 2ex;
+      }
+    </style>
+    </head>
+  <body>
+    <!-- main page content -->
+    <div id="content_div">
+      <!-- page header -->
+      <div id="header_div" style="background-color:#99badd;color:#ffffff;">
+        <canvas id="logologo" style="height:50px;width:600px;position:relative;" height="50" width="600"></canvas>
+<div id="linkbar-upper" style="background-color:#990000;"><div class="standard"><a href="/research.php" target="_top" data:bgcolor="#7bafd4">research</a></div><div class="standard"><a href="/experience.php" target="_top" data:bgcolor="#ffe800">experience</a></div><div class="active"><a href="/about.php" target="_top" data:bgcolor="#990000">about</a></div><div class="standard"><a href="/current.php" target="_top" data:bgcolor="#aa7733">current</a></div><div id="spacer"></div></div></div>
+
+<div id="content-desktop" class="tab">
+  <a href="about.php"><button class="tablinks" onclick="openTab(event, 'about')">About</button></a>
+  <a href ="topics.php"><button class="tablinks" onclick="openTab(event, 'experience')">Topics</button></a>
+  <a href ="games.php"><button class="tablinks" onclick="openTab(event, 'essays')">Games</button></a>
+
+</div>
+
+<script>
+function openTab(evt, name) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(name).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+</script>
+<script>
+document.getElementById('answer').value
+</script>
+
+
+<link rel='stylesheet' type='text/css' href='../paginator3000.css' />
+<script type='text/javascript' src='../paginator3000.js'></script>
+<style type='text/css'>
+.c { font-family: Georgia, 'Times New Roman', Times, serif; font-size: 11px; color: #000000; LETTER-SPACING: 0px; }
+A { text-decoration: none; }
+td { padding: 1px; }
+table { border-spacing: 1px 1px; }
+</style>
+<div class='wrapper'>
+<div class='content'>
+<table cellspacing=0 border=0 width=100%>
+<col width='33%'>
+<col width='70%'>		
+<tr><caption><font color=black><h1>Physics Through Games</h1></font><br>
+Find ways to teach physics in fun ways in the classroom. Search by topic, game, or keyword.</caption>
+
+<td></td><td nowrap><form name ='libgen' action='search.php'>
+	<input autofocus='autofocus' name='req' id='searchform' size=60 maxlength=200 value=''>
+<input class type=submit onclick='this.disabled='disabled'; document.forms.item(0).submit(); value='Search'><br>
+
+</body></html>
